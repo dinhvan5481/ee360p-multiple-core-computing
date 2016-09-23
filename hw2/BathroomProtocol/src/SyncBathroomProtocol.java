3c9cfbbda94fd33cb53f48e690f4282d0065d0e6
Van Quach <dinhvan5481@gmail.com>
2016-09-23 09:47:10 -0500

    Finished simple Sync Bathroom Protocol

--- hw2/BathroomProtocol/src/SyncBathroomProtocol.java
+++ hw2/BathroomProtocol/src/SyncBathroomProtocol.java
@@ -3,21 +3,50 @@
 // bathroom protocol
 
 public class SyncBathroomProtocol implements BathroomProtocol {
+  private int numberOfPeople;
+  private int numberOfFemale;
+  private int numberOfMale;
 
-  public void enterMale() {
-
+  public SyncBathroomProtocol() {
+    numberOfPeople = 0;
+    numberOfFemale = 0;
+    numberOfMale = 0;
   }
 
-  public void leaveMale() {
-
+  public synchronized void enterMale() {
+    while(numberOfFemale > 0) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    numberOfMale++;
   }
 
-  public void enterFemale() {
-
+  public synchronized void leaveMale() {
+    numberOfMale--;
+    if(numberOfMale == 0) {
+      notifyAll();
+    }
   }
 
-  public void leaveFemale() {
+  public synchronized void enterFemale() {
+    while (numberOfMale > 0) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    numberOfFemale++;
+  }
 
+  public synchronized void leaveFemale() {
+    numberOfFemale--;
+    if(numberOfFemale == 0) {
+      notifyAll();
+    }
   }
 
 
