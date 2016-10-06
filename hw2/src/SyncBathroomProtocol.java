3afe628ddb073ba72d1d122a3a866d7fd2a28087
SuperAndyBuilds <andyyang0114@gmail.com>
2016-10-06 13:56:49 -0500

    removed print statements

--- hw2/src/SyncBathroomProtocol.java
+++ hw2/src/SyncBathroomProtocol.java
@@ -13,47 +13,36 @@ public class SyncBathroomProtocol implements BathroomProtocol {
     public synchronized void enterMale() {
             while (countFemale != 0) {
                 try {
-                    System.out.println("before male wait");
                     this.wait();
-                    System.out.println("after male wait");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             countMale++;
-        System.out.println("male enters");
     }
 
     public synchronized void leaveMale() {
         countMale--;
-        System.out.println("male leaves");
         if (countMale == 0) {
             notifyAll();
-            System.out.println("after notify when no males in the room");
         }
     }
 
     public synchronized void enterFemale() {
         while (countMale!=0) {
             try {
-                System.out.println("before female wait");
                 this.wait();
-                System.out.println("after female wait");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
         countFemale++;
-        System.out.println("female enters");
     }
 
     public synchronized void leaveFemale() {
         countFemale--;
-        System.out.println("female leaves");
         if (countFemale == 0) {
             notifyAll();
-            System.out.println("after notify when no female is in the room.");
         }
     }
 }
