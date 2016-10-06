3dd5d73c73f8b5dd2764b984fee31a22ec7229c2
Van Quach <dinhvan5481@gmail.com>
2016-10-06 14:28:40 -0500

    Working on Q5

--- test/test2/src/FIFOLock.java
+++ test/test2/src/FIFOLock.java
@@ -1,22 +1,49 @@
 package test.test2;
 
+import java.util.PriorityQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by quachv on 9/30/2016.
  */
 public class FIFOLock {
     private int currentMinTicketNumberInWaitingQueue;
+    private ReentrantLock mutex = new ReentrantLock();
+    private Condition roomReadyCond = mutex.newCondition();
+
     private int currentTicket = 0;
+    private PriorityQueue<Integer> waitingQueue = new PriorityQueue<>();
+    private boolean isRoomOccupied = false;
+
 
     public synchronized int getTicket() {
         return ++currentTicket;
     }
 
-    public synchronized void requestCS(int ticketNumer) {
+    public synchronized void requestCS(int ticketNumer) throws InterruptedException {
+        if(!isRoomReadyFor(ticketNumer)) {
+            waitingQueue.add(ticketNumer);
+            while (!isRoomReadyFor(ticketNumer)) {
+                roomReadyCond.await();
+            }
+        }
+        isRoomOccupied = true;
 
 
     }
 
-    public synchronized void releaseCS() {
+    private boolean isRoomReadyFor(int ticketNumer) {
+        return !isRoomOccupied && ticketNumer < currentMinTicketNumberInWaitingQueue;
+    }
 
+    public synchronized void releaseCS() {
+        // update CS counter
+        isRoomOccupied = false;
+        // get next ticket number
+        // TODO: need to handle when
+        currentMinTicketNumberInWaitingQueue = waitingQueue.poll();
+        // signal waiting threads
+        roomReadyCond.signalAll();
     }
 }
