537f9a744d0146658debcfc27e5e8f8c7575da7a
Guillermo Leon <gleonoliva@gmail.com>
2016-10-05 21:46:32 -0500

    Added sampleTest templates

---
+++ hw2/sampleTest/MyLock.java
@@ -0,0 +1,24 @@
+// Q.3 (15 points) Implement the class myLock to coordinate entry 
+// into a room by various threads. There are two types of threads 
+// in the system - type1 and type2. The only requirement is that 
+// there should not be two threads of different types in the room 
+// at any time. Implement the class myLock with three methods void 
+// request1(), void request2() and release(). Threads of type 1 use 
+// request1() and threads of type 2 use request2() to enter the room. 
+// All threads use release() to exit the room. You do not have to 
+// worry about starvation.
+
+public class MyLock {
+
+    public void request1() {
+        // TODO
+    }
+
+    public void request2() {
+        // TODO
+    }
+
+    public void release() {
+        // TODO
+    }
+}
