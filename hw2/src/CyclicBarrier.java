d9e1aa29c065964fc247ff9853acfe8caed844e4
Guillermo Leon <gleonoliva@gmail.com>
2016-10-06 19:45:09 -0500

    Added some comments to help explain the code.

--- hw2/src/CyclicBarrier.java
+++ hw2/src/CyclicBarrier.java
@@ -7,35 +7,43 @@ import java.util.concurrent.Semaphore;
 public class CyclicBarrier {
     int parties = 0;
     int originalParties = 0;
-    int numRelease = 0;
-    Semaphore mulex = new Semaphore(1);
-    Semaphore mulex2 = new Semaphore(1);
-    Semaphore lock = new Semaphore (0);
+    int numRelease = 0; // Keep track of threads that leave await()
+    Semaphore mulex = new Semaphore(1); // Mutex for parties
+    Semaphore mulex2 = new Semaphore(1); // Mutex for number of released threads.
+    Semaphore lock = new Semaphore (0);  // Barrier 
     Semaphore extra;
 
     public CyclicBarrier(int parties) {
         this.parties = parties;
         this.numRelease = parties;
         this.originalParties = parties;
-        this.extra = new Semaphore (parties);
+        this.extra = new Semaphore (parties); // Required to allow only #parties at once.
     }
 
+    // We solve this by doing a 4 step process.
+    // 1. Allow only the number of threads specified in parties.
+    // 2. All threads but the last one hold execution.
+    // 3. Last one releases the rest of the threads waiting, now we count released threads.
+    // 4. Last one to be released, resets to initial state and releases 'parties' number of threads
+    //    so the process can continue.
     public int await() throws InterruptedException {
         extra.acquire();
         int arrivalIndex = parties;
         if (parties > 0){
             mulex.acquire(); //for mutual exclusion while changing the index
             parties--;
-            arrivalIndex = parties;
+            arrivalIndex = parties; // Guarantees each thread has its own arrival number.
             mulex.release();
 
             if (parties==0) {
+                // Last to arrive is responsible for telling the others waiting.
                 lock.release(); //release lock
                 mulex2.acquire();
                 numRelease--;
                 mulex2.release();
             } else {
-                lock.acquire(); //wait before the last one has gone through
+                // The first (parties - 1) threads will take this way and
+                lock.acquire(); // wait before the last one has gone through
                 mulex2.acquire();
                 if (numRelease >0) {
                     lock.release(); //release lock for the next thread
@@ -45,6 +53,8 @@ public class CyclicBarrier {
             }
             
             if (numRelease == 0) {
+                // Last one to leave the await() needs to reset variables
+                // so it can be reused.
                 parties = originalParties;
                 numRelease = originalParties;
                 extra.release(parties);
