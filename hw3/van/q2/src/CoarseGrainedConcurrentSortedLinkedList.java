71efbdab9d948824f02cb4d4cad1975b2e741d8d
Van Quach <dinhvan5481@gmail.com>
2016-10-27 15:26:36 -0500

    Working on template

---
+++ hw3/van/q2/src/CoarseGrainedConcurrentSortedLinkedList.java
@@ -0,0 +1,23 @@
+/**
+ * Created by quachv on 10/24/2016.
+ */
+public class CoarseGrainedConcurrentSortedLinkedList {
+    private int numOfThreads;
+
+    public CoarseGrainedConcurrentSortedLinkedList(int nThreads){
+        this.numOfThreads = nThreads;
+    }
+
+    public boolean add(int x) {
+        return false;
+    }
+
+    public boolean remove(int x) {
+        return false;
+    }
+
+    public boolean contains(int x) {
+        return false;
+    }
+
+}
---
+++ hw3/van/q2/src/IConcurrentSortedLinkedList.java
@@ -0,0 +1,8 @@
+/**
+ * Created by quachv on 10/24/2016.
+ */
+public interface IConcurrentSortedLinkedList {
+    boolean add(int x);
+    boolean remove(int x);
+    boolean contains(int x);
+}
