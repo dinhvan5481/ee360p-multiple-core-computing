e242c8afff58900216b005ef4ad3fde220cf82ba
Guillermo Leon <gleonoliva@gmail.com>
2016-10-29 15:12:56 -0500

    Added a way to get perf metrics.
    With 1, 2, 4 and 8 threads

--- hw3/guillermo/q1/matrix_mult.c
+++ hw3/guillermo/q1/matrix_mult.c
@@ -1,6 +1,47 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void print_matrix(int **matrix, int rows, int cols) {
+    int r, c;
+    printf("%d %d\n", rows, cols);
+    for(r = 0; r < rows; r++) {
+        for(c = 0; c < cols; c++){
+            printf((c == cols - 1)? "%d\n" : "%d ", matrix[r][c]);
+        }
+    }
+} 
+
+void load_matrix(FILE *file, int ***matrix, int *rows, int *cols) {
+    fscanf(file, "%d %d", rows, cols);
+    *matrix = malloc(*rows * sizeof(int*));
+    int r, c;
+    for(r = 0; r < *rows; r++) {
+        (*matrix)[r] = malloc(*cols * sizeof(int));
+        for(c = 0; c < *cols; c++) {
+            int value = 0;
+            fscanf(file, "%d", &value);
+            (*matrix)[r][c] = value;
+        }
+    }
+}
+
+void mult_matrix(int **matrix1, int mat1_rows, int mat1_cols, int **matrix2, int mat2_rows, int mat2_cols, int ***matrix3) {
+    int m3_r = mat1_rows, m3_c = mat2_cols;
+    int r, k;
+    #pragma omp parallel for
+    for(r = 0; r < m3_r; r++) {
+        int k = 0, j;
+        for(k = 0; k < m3_c; k++) {
+            int sum = 0;
+            for(j = 0; j < mat2_cols; j++) {
+                sum += matrix1[r][j] * matrix2[j][k];
+            }
+            (*matrix3)[r][k] = sum;
+        }
+    }
+}
 
 int main(int argc, char **argv) {
 
@@ -36,24 +77,10 @@ int main(int argc, char **argv) {
     int m1_r, m1_c;
     int m2_r, m2_c;
     int r, c;
+    clock_t start_time, end_time;
 
-    fscanf(file1, "%d %d", &m1_r, &m1_c);
-    matrix1 = malloc(m1_r * sizeof(int*));
-    for(r = 0; r < m1_r; r++) {
-        matrix1[r] = malloc(m1_c * sizeof(int));
-        for(c = 0; c < m1_c; c++) {
-            fscanf(file1, "%d", &matrix1[r][c]);
-        }
-    }
-
-    fscanf(file2, "%d %d", &m2_r, &m2_c);
-    matrix2 = malloc(m2_r * sizeof(int*));
-    for(r = 0; r < m2_r; r++) {
-        matrix2[r] = malloc(m2_c * sizeof(int));
-        for(c = 0; c < m2_c; c++) {
-            fscanf(file2, "%d", &matrix2[r][c]);
-        }
-    }
+    load_matrix(file1, &matrix1, &m1_r, &m1_c);
+    load_matrix(file2, &matrix2, &m2_r, &m2_c);
     
     fclose(file1);
     fclose(file2);
@@ -64,23 +91,29 @@ int main(int argc, char **argv) {
         m3[r] = malloc(m3_c * sizeof(int));
     }
 
-    #pragma omp parallel for
-    for(r = 0; r < m3_r; r++) {
-        int k = 0, j;
-        for(k = 0; k < m3_c; k++) {
-            int sum = 0;
-            for(j = 0; j < m2_c; j++) {
-                sum += matrix1[r][j] * matrix2[j][k];
+    if(thread_to_use == 0) {
+        const int THREAD_OPTIONS = 4; 
+        int threads[] = {1, 2, 4, 8};
+        
+        int times = 1000;
+        int t_count, thread_index;
+        clock_t time;
+        for(thread_index = 0; thread_index < THREAD_OPTIONS; thread_index++) {
+            time = 0;
+            omp_set_num_threads(threads[thread_index]);
+            for(t_count = 0; t_count < times; t_count++) {
+                start_time = clock();
+                mult_matrix(matrix1, m1_r, m1_c, matrix2, m2_r, m2_c, &m3);
+                end_time = clock();
+                time += end_time - start_time;
             }
-            m3[r][k] = sum;
+            printf("%d, %f\n", threads[thread_index], (float) (time) / CLOCKS_PER_SEC);
         }
-    }
-    
-    printf("%d %d\n", m3_r, m3_c);
-    for(r = 0; r < m3_r; r++) {
-        for(c = 0; c < m3_c; c++){
-            printf((c == m3_c - 1)? "%d\n" : "%d ", m3[r][c]);
-        }
+    } else {
+        omp_set_num_threads(thread_to_use);
+        mult_matrix(matrix1, m1_r, m1_c, matrix2, m2_r, m2_c, &m3);
+        print_matrix(m3, m3_r, m3_c);
     }
 
+    return 0;
 }
