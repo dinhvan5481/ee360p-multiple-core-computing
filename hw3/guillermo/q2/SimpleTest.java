a2d611c74575ae828d7701a1430990cdbb6eabaf
Van Quach <dinhvan5481@gmail.com>
2016-11-04 22:35:46 -0500

    Added test case

---
+++ hw3/guillermo/q2/SimpleTest.java
@@ -0,0 +1,96 @@
+import java.util.Random;
+
+/**
+ * Created by dinhvan5481 on 10/28/16.
+ */
+public class SimpleTest {
+    public static void main(String[] args) throws InterruptedException {
+        final int numThreads = 4;
+        final int numThreadRemove = 2;
+        final int max = 500;
+        final int min = 1;
+        final int numValues = 100;
+        final int numRun = 1000;
+        BagValues bagValues = new BagValues(min, max, numValues);
+        final Thread[] threads = new Thread[numThreads];
+
+        log("Case 1: happy path");
+        ListSet listUT = new FineGrainedListSet();
+        for (int i = numThreadRemove; i < numThreads; i++) {
+            threads[i] = new Thread(new TestRunner(listUT, bagValues, TestRunner.OperationType.Add, numRun));
+            threads[i].start();
+        }
+        for (int i = 0; i < numThreadRemove; i++) {
+            threads[i] = new Thread(new TestRunner(listUT, bagValues, TestRunner.OperationType.Remove, numRun / 100));
+            threads[i].start();
+        }
+        for (int i = 0; i < numThreads; i++) {
+            threads[i].join();
+        }
+
+
+        //TODO: not testing orders of the list
+
+        log(listUT.toString());
+        log("End case 1");
+
+
+    }
+
+    public static void log(String msg) {
+        System.out.println(msg);
+    }
+}
+
+class TestRunner implements Runnable {
+    public static enum OperationType {
+        Add,
+        Remove
+    }
+    private ListSet sut;
+    private OperationType op;
+    private BagValues bagValues;
+    private int numRun;
+
+    public TestRunner(ListSet linkedList, BagValues bagValues, OperationType op, int numRun) {
+        this.sut = linkedList;
+        this.op = op;
+        this.bagValues = bagValues;
+        this.numRun = numRun;
+    }
+
+    @Override
+    public void run() {
+        if(this.op == OperationType.Add) {
+            log("Adding thread");
+            int data = 0;
+            for (int i = 0; i < this.numRun; i++) {
+                data = bagValues.getValueAndMarkAdded();
+                if(this.sut.add(data)) {
+                    if(!(this.sut.contains(data) && bagValues.isAdded(data))) {
+                        log(String.format("Maybe failed. Added %d, but not exist in the list. Posibly another thread remove it", data));
+                        log(sut.toString());
+                    }
+                }
+            }
+        } else if (this.op == OperationType.Remove) {
+            log("Removing thread");
+            int data = 0;
+            for (int i = 0; i < this.numRun; i++) {
+                data = bagValues.getValue();
+                if(this.sut.contains(data)) {
+                    if(!this.sut.remove(data)) {
+                        log(String.format("Failed. Remove exist not success %d", data));
+                        log(sut.toString());
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    private void log(String msg) {
+        System.out.println(msg);
+    }
+}
